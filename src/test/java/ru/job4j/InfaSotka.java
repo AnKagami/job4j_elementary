f0cbfcb7fce1debe31ced9ddf7dd65afbf47baec
Kagami <grudinina.an22@gmail.com>
2025-03-20 19:44:25 +0800

    Commit +

---
+++ src/test/java/ru/job4j/InfaSotka.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class InfaSotka {
+    public static void main(String[] args) {
+        System.out.println("О великий и могучий Паша! Дай мне отдохнуть два часика");
+    }
+}
